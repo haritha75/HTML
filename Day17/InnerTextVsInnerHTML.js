900d0643131b9a9dbbdb1f31900a8c516a19c0ca
Yerukondu Haritha <yerukonduharitha@gmail.com>
2023-09-11 08:12:09 +0530

    Day17 work done

---
+++ Day17/InnerTextVsInnerHTML.js
@@ -0,0 +1,4 @@
+let element = document.getElementsByTagName("h1");
+
+element[0].innerText="<h1>Rock the show</h1>"; //it we are updating text only if you use html tags not effect to the text whatever we are write it disaly that one only.
+element[1].innerHTML="<h1><marquee>Rock</marquee></h1>"; //here whatever we write the text and also if you use html tags then that tags are apply to the text also.
---
+++ Day17/updatingAndSetttingAttributes.js
@@ -0,0 +1,36 @@
+function hello() {
+
+    let heading= document.getElementsByTagName("h1")[0];
+
+    heading.title="rock";
+}
+let i=0;
+
+function fun() {
+
+    let image=document.getElementsByTagName("img")[0];
+    if(i%2==0)
+
+      image.width='100';
+    else 
+      image.width='500';
+
+    i++;  
+
+
+}
+
+function fun1() {
+
+    let image1=document.getElementsByTagName('img')[1];
+    image1.src="./500720.jpg";
+    image1.width="500";
+    image1.alt="wall paper for laps";
+
+}
+
+function fun2() {
+
+    let image3=document.getElementsByTagName('img')[2];
+    image3.setAttribute('alt',"hello");
+}
